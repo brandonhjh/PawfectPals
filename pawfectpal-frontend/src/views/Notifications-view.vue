f8ce366dc34617435f7ad7032a6f4844d5aabf81
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-06-19 20:47:50 +0800

    Update Notifications-view.vue
    notification page done-e

--- pawfectpal-frontend/src/views/Notifications-view.vue
+++ pawfectpal-frontend/src/views/Notifications-view.vue
@@ -1,87 +1,78 @@
 <template lang="html">
-    <div>
-        <h1>Notifications</h1>
+  <div>
+    <h3 class="p-1 text-center text decoration underline">Notifications</h3 >
+  </div>
+
+  <section class="section-50 ">
+    <div class="container bg-white text-start ">
+      <h5 class="col-sm-12 my-1">Today <i class="fa fa-bell text-muted"></i></h5>
     </div>
-    <section class="section-50">
-        <div class="container">
-            <h3 class="m-b-50 heading-line">Today <i class="fa fa-bell text-muted"></i></h3>
-        </div>
 
-        <div class="notification-ui_dd-content">
-            <div class="notification-list notification-list--unread">
-                <div class="notification-list_content">
-                    
-                    <div class="notification-list_detail">
-                        <p><b>Leo Medical Appointment </b></p>
-                        <p class="text-muted">Send for medical appt, rmb to bring medicine</p>
-                        
-                    </div>
-
-                    <div class="notification-time">
-                        <p class="text-muted"><b>8:00 pm</b></p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="notification-list notification-list--unread">
-                <div class="notification-list_content">
-                    
-                    <div class="notification-list_detail">
-                        <p><b>Milo Dinner</b> </p>
-                        <p class="text-muted">Try new organic kebble</p>
-                        
-                    </div>
-                    <div class="notification-time">
-                        <p class="text-muted"><b>6:30 pm</b></p>
-                    </div>
-                </div>
-                
-            
-                
-            </div>
-                <div class="container">
-                <h3 class="m-b-50 heading-line">Yesterday<i class="fa fa-bell text-muted"></i></h3>
-            </div>
-
-            <div class="notification-list">
-                <div class="notification-list_content">
-                    
-                    <div class="notification-list_detail">
-                        <p><b>Zorro Vet Check-up</b> </p>
-                        <p class="text-muted">appointment @ Brighton Vet, Serangoon </p>
-
-                    </div>
-                    <div class="notification-time">
-                        <p class="text-muted"><b>7:00 pm</b> </p>
-                    </div>
-                </div>
-                
-            </div>
-            <div class="notification-list">
-                <div class="notification-list_content">
-                    
-                    <div class="notification-list_detail">
-                        <p><b>Change Leo litter</b> </p>
-                        <p class="text-muted">Clear litter box, change the sand.</p>
-                        
-                    </div>
-                    <div class="notification-time">
-                        <p class="text-muted"><b>10:00 am</b></p>
-                    </div>
-                    
-                </div>
-                
-            </div>
-
-            <div class="text-center">
-            <a href="#!" class="dark-link">Load more activity</a>
-            </div>
-            
-        </div>
+    
+      
         
-    </section>
-</template>
+          <div class="container my-1">
+            <div class="row rounded border border-muted bg-white ">
+              <div class="col-8 p-2">
+                <div class="p-1 bg-white text-start  ">
+                  <b>Leo Medical Appointment </b>
+                </div>
+                <p class="text-muted text-start">
+                  Send for medical appt, rmb to bring medicine
+                </p>
+              </div>
+              <div class="col-4 ">
+                <div class="p-2 my-1 bg-white text-dark text-end "><small>8:00 pm</small></div>
+              </div>
+            </div>
 
+          </div>
+        
+          <div class="container my-1">
+            <div class="row rounded border border-muted bg-white ">
+              <div class="col-8 p-2">
+                <div class="p-1 bg-white text-start  ">
+                  <b>Milo Medical Appointment </b>
+                </div>
+                <p class="text-muted text-start">
+                  Send for medical appt, rmb to bring medicine
+                </p>
+              </div>
+              <div class="col-4 ">
+                <div class="p-2 my-1 bg-white text-dark text-end "><small>8:00 pm</small></div>
+              </div>
+            </div>
+
+          </div>
+      
+    
+
+    <div class="container bg-8b9d83 text-start">
+      <h5 class="col-sm-12 my-1">Yesterday <i class="fa fa-bell text-muted"></i></h5>
+    </div>
+
+    
+          <div class="container my-1 ">
+            <div class="row rounded border border-muted bg-white ">
+              <div class="col-8 p-2">
+                <div class="p-1 bg-white text-dark text-start">
+                  <b>Zorro Vet Appointment</b>
+                </div>
+                <p class="text-muted text-start">Brighton Vet Clinic @Serangoon</p>
+              </div>
+              <div class="col-4">
+                <div class="p-2 bg-white text-dark text-end"><small>7:00 pm</small></div>
+              </div>
+            </div>
+
+          </div>
+        
+
+    <div class="text-center">
+      <a href="#!" class="dark-link">Load more activity</a>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
@@ -90,7 +81,5 @@ export default {
 };
 </script>
 
-
-<style lang="css" scoped>
-
+<style lang="css">
 </style>
