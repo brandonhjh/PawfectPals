5f462a6b6e345371597ed2ba036bd7e466a9ea69
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-06-19 23:14:52 +0800

    Update Groups-view.vue
    group page first draft eaint

--- pawfectpal-frontend/src/views/Groups-view.vue
+++ pawfectpal-frontend/src/views/Groups-view.vue
@@ -1,14 +1,50 @@
 <template lang="html">
-    <div class="ms-auto text-start">
-        <h1>Groupy toupy</h1>
+    <div class="container my-3 ">
+            <div class="row">
+              <div class="col-5 p-2 ">
+                <div class="p-1 rounded text-center">
+                  <button type="button" class="btn btn-outline-success my-1 "> + Join Group </button>
+                </div>
+              </div>
+              <div class="col-2 ">
+              </div>
+              <div class="col-5 p-2">
+                <div class="p-1 rounded text-center">
+                  <button type="button" class="btn btn-outline-success my-1 "> + Create Group </button>
+                </div>
+              </div>
+            </div>
+    </div>
+
+          
+    <div class="container mr-2 pr-2 rounded rounded-top text-center bg-success">
+        <div class="row my-1">
+            <div class="col-1 p-1 my-5 ">
+                <img src="../assets/house.svg" alt="" class="d-flex flex-row justify-content-between align-items-center w-100 ">
+                
+            </div>
+            <div class="col-4 my-5 text-light" style="display: flex; align-items: center;"><b>Hong Family</b></div>
+            <div class="col-3 p-1 my-5 "></div>
+            <div class="col-2 p-1 my-2 " style="display: flex; align-items: center;">
+                <button type="button" class="btn btn-outline-light my-1 ">See Pets </button>
+                    
+               
+            </div>
+            <div class="col-2 p-1 my-2 " style="display: flex; align-items: center;">
+                
+                    
+                <button type="button" class="btn btn-outline-light my-1">See members </button>
+            </div>
+           
+        </div>
+    
     </div>
 </template>
 <script>
 export default {
-    name: "Group-view",
-    components: {},
-}
+  name: "Group-view",
+  components: {},
+};
 </script>
 <style lang="css">
-    
 </style>
