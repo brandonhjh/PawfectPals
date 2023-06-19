5c4a35c6c6a7d0807b697a452fc0a37af0048d9b
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-06-19 21:34:42 +0800

    Merge branch 'notif-page-creation-E' of https://github.com/smu-hack-dsc/Team-23-Project into notif-page-creation-E

---
+++ pawfectpal-frontend/src/views/Create-pets-view.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: "Create-pet-view",
+    components: {},
+}
+</script>
+<style lang="css">
+    
+</style>
