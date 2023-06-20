ba5908cc56ff2a558b411513458c5ab383278659
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-06-20 20:59:05 +0800

    Update Groups-view.vue
    second draft group view

--- pawfectpal-frontend/src/views/Groups-view.vue
+++ pawfectpal-frontend/src/views/Groups-view.vue
@@ -23,7 +23,30 @@
                 <img src="../assets/house.svg" alt="" class="d-flex flex-row justify-content-between align-items-center w-100 ">
                 
             </div>
-            <div class="col-4 my-5 text-light" style="display: flex; align-items: center;"><b>Hong Family</b></div>
+            <div class="col-3 my-5 text-light" style="display: flex; align-items: center;"><b>Hong Family</b></div>
+            <div class="col-3 p-1 my-5 "></div>
+            <div class="col-2 p-1 my-2 " style="display: flex; align-items: center;">
+                <button type="button" class="btn btn-outline-light my-1 ">See Pets </button>
+                    
+               
+            </div>
+            <div class="col-2 p-1 my-2 " style="display: flex; align-items: center;">
+                
+                    
+                <button type="button" class="btn btn-outline-light my-1">See members </button>
+            </div>
+           
+        </div>
+    
+    </div>
+
+    <div class="container mr-2 pr-2 rounded rounded-top text-center bg-success">
+        <div class="row my-1">
+            <div class="col-1 p-1 my-5 ">
+                <img src="../assets/house.svg" alt="" class="d-flex flex-row justify-content-between align-items-center w-100 ">
+                
+            </div>
+            <div class="col-3 my-5 text-light" style="display: flex; align-items: center;"><b>Home</b></div>
             <div class="col-3 p-1 my-5 "></div>
             <div class="col-2 p-1 my-2 " style="display: flex; align-items: center;">
                 <button type="button" class="btn btn-outline-light my-1 ">See Pets </button>
