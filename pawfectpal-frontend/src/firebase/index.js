1c14af7c6627f36d67f048ccbceefa7e23696b5b
choyboys <jared.choy777@gmail.com>
2023-06-21 14:17:09 +0800

    login and states done
    added vuex and firebase to npm
    created store and firebase configs
    use firebase to manage log in and vuex to manage states

---
+++ pawfectpal-frontend/src/firebase/index.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAbp9cWuGC7WDSt7Q19hwDuwUVEe6YHs4Q",
+  authDomain: "pawfect-pals-da18a.firebaseapp.com",
+  databaseURL:
+    "https://pawfect-pals-da18a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pawfect-pals-da18a",
+  storageBucket: "pawfect-pals-da18a.appspot.com",
+  messagingSenderId: "458110592532",
+  appId: "1:458110592532:web:13da8b1eab5a6b6a6ea901",
+  measurementId: "G-WGJQ48HVDL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
