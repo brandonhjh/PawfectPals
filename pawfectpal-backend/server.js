043a635fb73315da63bc47a0cca4536a6697a635
QiJie53 <limqijie53@gmail.com>
2023-07-10 16:12:44 +0800

    separated sample input data

--- pawfectpal-backend/server.js
+++ pawfectpal-backend/server.js
@@ -5,6 +5,7 @@ const firebase = require('firebase/app');
 require('firebase/database');
 
 const { getDatabase, onValue, ref, set: databaseSet, push, set, child } = require('firebase/database');
+const { executeQueries, addDataToFirebase } = require('./firebaseData');
 
 const app = express();
 
@@ -15,8 +16,7 @@ app.use(cors());
 const firebaseConfig = {
   apiKey: "AIzaSyAbp9cWuGC7WDSt7Q19hwDuwUVEe6YHs4Q",
   authDomain: "pawfect-pals-da18a.firebaseapp.com",
-  databaseURL:
-    "https://pawfect-pals-da18a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://pawfect-pals-da18a-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "pawfect-pals-da18a",
   storageBucket: "pawfect-pals-da18a.appspot.com",
   messagingSenderId: "458110592532",
@@ -32,116 +32,7 @@ app.get('/', (req, res) => {
   res.send('Hello, world! testing');
 });
 
-function executeQueries(queries) {
-  const updates = {};
-
-  queries.forEach((query) => {
-    updates[query.ref] = query.data;
-  });
-
-  return databaseSet(ref(database), updates);
-}
-
-function addDataToFirebase() {
-  executeQueries([
-    {
-      ref: 'user',
-      data: {
-        brandon: { password: 'apple123', userPicture: null },
-        jennifer: { password: 'orange456', userPicture: null },
-        michael: { password: 'banana789', userPicture: null }
-      }
-    },
-    {
-      ref: 'groups',
-      data: {
-        '100': { groupName: 'Safe Space' },
-        '101': { groupName: 'Study Group' },
-        '102': { groupName: 'Fitness Club' }
-      }
-    },
-    {
-      ref: 'userGroup',
-      data: {
-        '100': { brandon: true },
-        '101': { brandon: true, jennifer: true },
-        '102': { michael: true, jennifer: true }
-      }
-    },
-    {
-      ref: 'pets',
-      data: {
-        Leo: { groupID: 100, breed: 'Husky', birthday: '2001-01-01', petPicture: null },
-        Bella: { groupID: 101, breed: 'Labrador', birthday: '2015-03-10', petPicture: null },
-        Max: { groupID: 101, breed: 'Golden Retriever', birthday: '2018-07-22', petPicture: null },
-        Charlie: { groupID: 102, breed: 'Bulldog', birthday: '2019-01-05', petPicture: null }
-      }
-    },
-    {
-      ref: 'tasks',
-      data: {
-        '200': {
-          groupID: 100,
-          taskDate: '2023-06-15',
-          taskTime: '12:00:00',
-          petName: 'Leo',
-          title: "Leo's dinner",
-          notes: 'Remember to top up food!',
-          isCompleted: false,
-          people: 'brandon,jennifer'
-        },
-        '201': {
-          groupID: 101,
-          taskDate: '2023-06-16',
-          taskTime: '14:30:00',
-          petName: 'Bella',
-          title: 'Walk in the park',
-          notes: 'Take Bella for a 30-minute walk',
-          isCompleted: false,
-          people: 'brandon'
-        },
-        '202': {
-          groupID: 101,
-          taskDate: '2023-06-18',
-          taskTime: '10:00:00',
-          petName: 'Max',
-          title: 'Training session',
-          notes: 'Train Max on basic commands',
-          isCompleted: false,
-          people: 'jennifer'
-        },
-        '203': {
-          groupID: 102,
-          taskDate: '2023-06-17',
-          taskTime: '16:00:00',
-          petName: 'Charlie',
-          title: 'Playtime at the park',
-          notes: 'Spend some time playing with Charlie',
-          isCompleted: false,
-          people: 'michael,jennifer'
-        }
-      }
-    },
-    {
-      ref: 'taskPeople',
-      data: {
-        '200': { groupID: 100, brandon: true, jennifer: true },
-        '201': { groupID: 101, brandon: true },
-        '202': { groupID: 101, jennifer: true },
-        '203': { groupID: 102, michael: true, jennifer: true }
-      }
-    }
-  ])
-    .then(() => {
-      console.log('Data added to Firebase Realtime Database successfully');
-    })
-    .catch((error) => {
-      console.error('Error adding data to Firebase Realtime Database:', error);
-    });
-}
-
-// Add data to Firebase Realtime Database when the server starts
-addDataToFirebase();
+// addDataToFirebase(database);
 
 // GET TASK from the database
 app.get('/GET/api/task', (req, res) => {
