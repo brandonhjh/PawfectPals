027fda939366c075752417e79557065dc87455b4
choyboys <jared.choy777@gmail.com>
2023-07-11 00:51:53 +0800

    shd be finalised

--- pawfectpal-frontend/src/views/Calendar-view.vue
+++ pawfectpal-frontend/src/views/Calendar-view.vue
@@ -1,6 +1,8 @@
 <template lang="html">
-  <div class="m-2 mb-0 pt-2">
-    <FullCalendar :options="calendarOptions" />
+  <div class="container border rounded-4 pt-4 px-1 pb-5 mt-5" id="green-banner">
+    <div class="mx-0 mb-5 mt-5 pt-2" style="color: white">
+      <FullCalendar :options="calendarOptions" />
+    </div>
   </div>
 </template>
 
@@ -9,7 +11,7 @@ import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
-import interactionPlugin from "@fullcalendar/interaction";
+// import interactionPlugin from "@fullcalendar/interaction";
 
 export default {
   components: {
@@ -18,14 +20,34 @@ export default {
   data: function () {
     return {
       calendarOptions: {
-        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin],
+        plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
         initialView: "dayGridMonth",
+        headerToolbar: {
+          left: "prev,next",
+          center: "title",
+          right: "dayGridMonth,dayGridWeek",
+        },
+        navLinks: true,
         weekends: true,
         events: [{ title: "", start: new Date("July 17, 2023 03:24:00") }],
-        themeSystem: "bootstrap5",
       },
     };
   },
 };
 </script>
-<style></style>
+<style lang="css">
+.fc-daygrid-day-number {
+  color: white;
+}
+.fc-col-header-cell-cushion {
+  color: white;
+}
+
+.fc-event-time {
+  color: white;
+}
+
+.fc-event-title {
+  color: white;
+}
+</style>
