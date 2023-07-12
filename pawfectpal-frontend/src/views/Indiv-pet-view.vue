3b5ce9a8925ed2aedc1595ae51da626d268fedc2
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-07-12 20:20:18 +0800

    changed the details from input form into text form

--- pawfectpal-frontend/src/views/Indiv-pet-view.vue
+++ pawfectpal-frontend/src/views/Indiv-pet-view.vue
@@ -19,41 +19,55 @@
 
         </div>
         <div class="row bg-white border rounded-3 mx-3 my-3 py-3 px-3 ">
-            <div class="input-group my-auto mx-auto p-2 mt-3">
-                <input type="text" class="form-control rounded-3" id="petDetail" placeholder="Birthday">
+            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                Birthday
             </div>
-            <div class="input-group my-auto mx-auto p-2">
-                <input type="text" class="form-control rounded-3" id="petDetail" placeholder="Breed">
+            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                Breed
             </div>
-            <div class="input-group my-auto mx-auto p-2">
-                <input type="text" class="form-control rounded-3" id="petDetail" placeholder="Weight">
+            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                Weight
             </div>
-            <div class="input-group my-auto mx-auto p-2">
-                <input type="text" class="form-control rounded-3" id="petDetail" placeholder="Medication">
-            </div>
-            <div class="input-group my-auto mx-auto p-2">
-                <label class="input-group-text pe-4">Yearly Checkup Done &nbsp;&nbsp;</label>
-                <select class="form-select" id="petCheckUp">
-                    <option selected>--</option>
-                    <option value="yes">Yes</option>
-                    <option value="no">No</option>
-                </select>
-            </div>
-            <div class="input-group my-auto mx-auto p-2">
-                <input type="text" class="form-control rounded-3 " id="petDetail" placeholder="Description" >
-            </div>
+            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                Medication
+            </div>    
+            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                Description
+            </div>  
+            
+            <div class="col-8 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark"> Yearly check-up done </div>
+            <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark text-center"> Yes </div>
             
             <div class="col d-flex justify-content-center my-auto mx-auto p-2">
                 <button
                     type="submit"
                     class="btn btn-outline-dark btn-sm"
                 >
-                Update
+                Edit
                 </button>
             </div>
+
         
 
         </div>
+
+        <div class="row my-auto mx-auto p-2 mt-3"><h5>Care Takers</h5>
+            
+        </div>
+        <div class="row ">
+            <div class="col-4">
+                <img src="https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=1000w" class="w-75 rounded-circle ps-2">
+            </div>
+            <div class="col-4">
+                <img src="https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=1000w" class="w-75 rounded-circle ps-2">
+            </div>
+        </div>
+        <div class="row my-auto mx-auto p-2 mt-3">
+            <div class="col-4">
+                Jared
+            </div>
+        </div>
+        
         
         
     </div>
