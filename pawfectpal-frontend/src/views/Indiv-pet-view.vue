20be8b8e8618b6ceac08a6970b54dceb711f266b
Eaint Hmu Aung <haeaint.2022@scis.smu.edu.sg>
2023-07-14 19:44:46 +0800

    done with the care taker and group

--- pawfectpal-frontend/src/views/Indiv-pet-view.vue
+++ pawfectpal-frontend/src/views/Indiv-pet-view.vue
@@ -35,12 +35,12 @@
                 Description
             </div>  
             
-            <div class="col-8 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark"> Yearly check-up done </div>
-            <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark text-center"> Yes </div>
+            <div class="col-8 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark"> Last check-up done </div>
+            <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark text-center"> 12/06/2023 </div>
             
             <div class="col d-flex justify-content-center my-auto mx-auto p-2">
                 <button
-                    type="submit"
+                    type="submit" 
                     class="btn btn-outline-dark btn-sm"
                 >
                 Edit
@@ -51,22 +51,31 @@
 
         </div>
 
-        <div class="row my-auto mx-auto p-2 mt-3"><h5>Care Takers</h5>
+        <div class="row my-2 mx-1 py-2 px-1"><h4>Group</h4>
             
         </div>
-        <div class="row ">
-            <div class="col-4">
-                <img src="https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=1000w" class="w-75 rounded-circle ps-2">
-            </div>
-            <div class="col-4">
-                <img src="https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=1000w" class="w-75 rounded-circle ps-2">
+
+        <div class="row mx-2 px-2 ">
+           <div id="font-dark-green" class="bg-light mx-auto p-2 border rounded-3 border-muted">
+                Home
             </div>
         </div>
-        <div class="row my-auto mx-auto p-2 mt-3">
-            <div class="col-4">
+        
+        <div class="row my-2 mx-1 py-2 px-1"><h4>Care Takers</h4>
+            
+        </div>
+
+        <div class="row mx-2 px-2 mb-4">
+           <div id="font-dark-green" class="bg-light mx-auto p-2 border rounded-3 border-muted">
                 Jared
             </div>
+            <div id="font-dark-green" class="bg-light mx-auto my-2 mb-5 p-2 border rounded-3 border-muted">
+                Brandon
+            </div>
         </div>
+
+
+       
         
         
         
