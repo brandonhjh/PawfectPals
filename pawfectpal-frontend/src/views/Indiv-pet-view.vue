65355eb810142bde270c5c62dc0dcd79be30315a
belinda <belindachan159@gmail.com>
2023-07-26 22:11:28 +0800

    Pets view done
    still got lines in between rows

--- pawfectpal-frontend/src/views/Indiv-pet-view.vue
+++ pawfectpal-frontend/src/views/Indiv-pet-view.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-    <div class="container-fluid border rounded-3 my-2 p-2" id="green-banner">
-        <div class="row my-2">
+    <div class="container-fluid border p-0" >
+        <div class="row mt-3 mx-2 pt-2 rounded-top-3" id="green-banner">
             <div class="col-4">
                 <img src="https://images.squarespace-cdn.com/content/v1/58b4791ad2b857c893179e34/1537971642021-LHW76T7O8JG0M4GLTSTP/IMG_2818.jpg?format=1000w" class="w-75 rounded-circle ps-2">
             </div>
@@ -18,62 +18,70 @@
             </div>
 
         </div>
-        <div class="row bg-white border rounded-3 mx-3 my-3 py-3 px-3 ">
-            <div id="font-dark-green" class="col-8 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted "> Birthday </div>
-            <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark text-center"> 01/06/2020 </div>
+        <div class="row mx-2 p-3 " id="green-banner">
+            <div class="bg-white rounded">
+                <div class="row m-0">
+                    <div id="font-dark-green" class="col-8 my-auto mx-auto p-2 mt-3 border rounded-start-3 border-muted "> Birthday </div>
+                    <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-end-3 border-muted text-dark text-center"> 01/06/2020 </div>
+                </div>
 
-            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
-                Breed
+                <div class="row m-0">
+                    <div id="font-dark-green" class="col-8 my-auto mx-auto p-2 mt-3 border rounded-start-3 border-muted "> Breed </div>
+                    <div id="font-dark-green" class="col-4 my-auto mx-auto p-2 mt-3 border rounded-end-3 border-muted"> Weight </div>
+                </div>
+                
+                <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted"> Description </div>  
+                
+                <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
+                    Medication
+                </div>    
+                
+                <div class="row m-0">
+                    <div id="font-dark-green" class="col-8 my-auto mx-auto p-2 mt-3 border rounded-start-3 border-muted"> Last check-up done </div>
+                    <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-end-3 border-muted text-dark text-center"> 12/06/2023 </div>
+                </div>
+
+                <div class="col d-flex justify-content-center my-auto mx-auto p-2">
+                    <button
+                        type="submit" 
+                        class="btn btn-outline-dark btn-sm"
+                        @click="goToPetUpdate"
+                    >
+                    Edit
+                    </button>
+                </div>
             </div>
-            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
-                Weight
-            </div>
-            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
-                Medication
-            </div>    
-            
-            
-            <div id="font-dark-green" class="col-8 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted"> Last check-up done </div>
-            <div class="col-4 my-auto mx-auto p-2 mt-3 border rounded-3 border-muted text-dark text-center"> 12/06/2023 </div>
-
-            <div id="font-dark-green" class="my-auto mx-auto p-2 mt-3 border rounded-3 border-muted">
-                Description
-            </div>  
-            
-            <div class="col d-flex justify-content-center my-auto mx-auto p-2">
-                <button
-                    type="submit" 
-                    class="btn btn-outline-dark btn-sm"
-                    @click="goToPetUpdate"
-                >
-                Edit
-                </button>
-            </div>
-
-        
-
+        </div>
+        <div class="row mx-2 py-2 px-1" id="green-banner">
+            <h4>Group</h4>
         </div>
 
-        <div class="row my-2 mx-1 py-2 px-1"><h4>Group</h4>
-            
-        </div>
-
-        <div class="row mx-2 px-2 ">
-           <div id="font-dark-green" class="bg-light mx-auto p-2 border rounded-3 border-muted">
-                Home
-            </div>
+        <div class="row mx-2 px-3 " id="green-banner">
+           <div class="bg-white rounded p-2 ps-3">
+            <p id="font-dark-green" class="m-auto"> Home </p>
+           </div>
         </div>
         
-        <div class="row my-2 mx-1 py-2 px-1"><h4>Care Takers</h4>
-            
+        <div class="row mx-2 py-2 px-1" id="green-banner">
+            <h4>Care Takers</h4>
         </div>
 
-        <div class="row mx-2 px-2 mb-4">
-           <div id="font-dark-green" class="bg-light mx-auto p-2 border rounded-3 border-muted">
-                Jared
+        <div class="row mx-2 px-2 mb-5 rounded-bottom-3" id="green-banner">
+            <div class="col-3 align-self-center text-center">
+                <div class="circle-singleline m-auto">
+                    J
+                </div>
+                <div class="mt-1">
+                    <p> Jared </p>
+                </div>
             </div>
-            <div id="font-dark-green" class="bg-light mx-auto my-2 mb-5 p-2 border rounded-3 border-muted">
-                Brandon
+            <div class="col-3 align-self-center text-center">
+                <div class="circle-singleline m-auto">
+                    B
+                </div>
+                <div class="mt-1">
+                    <p> Brandon </p>
+                </div>
             </div>
         </div>
 
